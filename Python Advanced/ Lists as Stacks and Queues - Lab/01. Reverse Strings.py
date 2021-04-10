4ee7f3bd7747a290cce6e2c159cbc9f48d129e24
Borislav Karaivanov <boriza777@gmail.com>
2021-04-10 20:07:14 +0300

    Delete Python Advanced/ Lists as Stacks and Queues - Lab directory

--- Python Advanced/ Lists as Stacks and Queues - Lab/01. Reverse Strings.py
+++
@@ -1,6 +0,0 @@
-text = input()
-stack = []
-for chr in text:
-    stack.append(chr)
-for i in range(len(stack)):
-    print(stack.pop(),end="")
--- Python Advanced/ Lists as Stacks and Queues - Lab/02. Matching Parentheses.py
+++
@@ -1,8 +0,0 @@
-expression = input()
-stack = []
-for i in range(len(expression)):
-    if expression[i] == "(":
-        stack.append(i)
-    elif expression[i] == ")":
-        start = stack.pop()
-        print(expression[start:i+1])
